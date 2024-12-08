85b349f2b458a85603b52993076ee193aff172a9
average <julianmuchacho666@gmail.com>
2024-12-08 03:02:32 -0500

    Completed day7advent in ruby

--- day7advent/test.rb
+++ day7advent/test.rb
@@ -14,16 +14,13 @@ class Test
     "Test Value: #{@test_value}, Numbers: #{@numbers.join(', ')}"
   end
 
+  # example: 4900956092109: 699 8 3 36 5 4 4 458 7 7
   def evaluate_test
     return false if @numbers.empty?
 
-    numbers.each_with_index { |_, index|
-      result = numbers[0]
-      selected_nums = numbers[1...index + 1]
-
-      return true if try(result,selected_nums)
-
-    }
+    starting_num = numbers[0]
+    selected_nums = numbers[1..]
+    return true if try(starting_num, selected_nums)
 
     false
 
@@ -32,37 +29,35 @@ class Test
   def try(current, selected_nums)
     return false if selected_nums.empty?
 
-    current_nums = selected_nums.dup
+    current_num = selected_nums.first
+    remaining_nums = selected_nums[1..-1]
 
-    while current_nums.any?
-      current_num = current_nums.shift
+    #addition
+    result_add = current + current_num
+    return true if result_add == @test_value && remaining_nums.length == 0
 
-      for op in [0,1]
-        result = test_operators(current, current_num, op)
+    try_add_path = try(result_add, remaining_nums)
+    return try_add_path if try_add_path
 
-        return true if result == @test_value
+    #mult
+    result_mult = current*current_num
+    return true if result_mult == @test_value && remaining_nums.length == 0
 
-        if try(result, current_nums)
-          return true
-        end
+    try_mult_path = try(result_mult, remaining_nums)
+    return try_mult_path if try_mult_path
 
+    #append
+    result_append = "#{current}#{current_num}".to_i
+    return true if result_append == @test_value && remaining_nums.length == 0
 
-      end
-
-    end
+    try_append_path = try(result_append, remaining_nums)
+    return try_append_path if try_append_path
 
     false
   end
 
-  def test_operators(current, other, op)
-    case op
-    when 0
-      return current + other
-    when 1
-      return current * other
-    end
-
-    current
+  def english_op(op)
+    op == 0 ? "+" : "*"
   end
 end
 
