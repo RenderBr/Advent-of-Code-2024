fe75f97001dc1043fee8d37d6964d3ecfa54c458
average <julianmuchacho666@gmail.com>
2024-12-08 03:05:51 -0500

    slight cleanup

--- day7advent/test.rb
+++ day7advent/test.rb
@@ -10,11 +10,6 @@ class Test
     @memo = {}
   end
 
-  def to_s
-    "Test Value: #{@test_value}, Numbers: #{@numbers.join(', ')}"
-  end
-
-  # example: 4900956092109: 699 8 3 36 5 4 4 458 7 7
   def evaluate_test
     return false if @numbers.empty?
 
@@ -32,21 +27,21 @@ class Test
     current_num = selected_nums.first
     remaining_nums = selected_nums[1..-1]
 
-    #addition
+    # addition
     result_add = current + current_num
     return true if result_add == @test_value && remaining_nums.length == 0
 
     try_add_path = try(result_add, remaining_nums)
     return try_add_path if try_add_path
 
-    #mult
-    result_mult = current*current_num
+    # mult
+    result_mult = current * current_num
     return true if result_mult == @test_value && remaining_nums.length == 0
 
     try_mult_path = try(result_mult, remaining_nums)
     return try_mult_path if try_mult_path
 
-    #append
+    # append
     result_append = "#{current}#{current_num}".to_i
     return true if result_append == @test_value && remaining_nums.length == 0
 
@@ -55,9 +50,4 @@ class Test
 
     false
   end
-
-  def english_op(op)
-    op == 0 ? "+" : "*"
-  end
 end
-
